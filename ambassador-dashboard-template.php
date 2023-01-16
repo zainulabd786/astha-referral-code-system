0c2315776d33a8a6c3dc2cb675e2b5594aad7ef8
Zainul Abideen <waeezazeem@gmail.com>
2023-01-17 02:54:33 +0530

    feat: adds my account and logout links on user dash

--- ambassador-dashboard-template.php
+++ ambassador-dashboard-template.php
@@ -7,6 +7,19 @@
 ?>
 
 <div class="p-4">
+    <nav id="site-navigation" class="main-navigation" aria-label="Top Menu">
+    <div class="menu-main-menu-container">
+        <ul id="top-menu" class="menu">
+            <li class="menu-item menu-item-type-post_type menu-item-object-page">
+                <a href="<?php echo get_edit_profile_url( ); ?>">My Account</a>
+            </li>
+            <li class="menu-item menu-item-type-post_type menu-item-object-page">
+                <a href="<?php echo wp_logout_url( get_permalink() ); ?>">Logout</a>
+            </li>
+        </ul>
+    </div>
+    </nav>
+
     <?= do_shortcode('[as_earnings_table]') ?>
 </div>
 
